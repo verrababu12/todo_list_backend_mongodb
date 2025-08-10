61dd698c5f175a1cb1881a62a95c9efd494faec4
veeru <veeru@gmail.com>
2025-08-10 13:33:44 +0530

    CHANGES

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
