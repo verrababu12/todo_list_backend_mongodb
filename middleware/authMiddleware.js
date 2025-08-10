0fd81517aa05e02e59c8c3fd71b6b40d605244d1
veeru <veeru@gmail.com>
2025-08-10 09:34:52 +0530

    result

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const authMiddleware = (req, res, next) => {
   }
   const token = authHeader.split(" ")[1];
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
     req.user = decoded; // { id: userId }
     next();
   } catch (err) {
